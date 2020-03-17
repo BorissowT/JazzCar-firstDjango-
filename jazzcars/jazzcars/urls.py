ead5ffb54831822346859aaa427a0293a871e2e5
tbs99 <timsregisters@gmail.com>
2020-03-18 00:57:54 +0300

    initial commit

--- jazzcars/jazzcars/urls.py
+++ jazzcars/jazzcars/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path
-from autos.views import MainView
+from autos.views import MainView, CategoryView, CarView
 
 urlpatterns = [
     path('', MainView.as_view()),
+    path('<str:category>/', CategoryView.as_view()),
+    path('category/<int:id>', CarView.as_view()),
 ]
